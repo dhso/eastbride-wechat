642e1837b78a04dd1dd551f6ef8c4412ed845c68
dhso <dhso@163.com>
2015-08-30 12:54:20 +0800

    ++

--- sql/init.sql
+++ sql/init.sql
@@ -10,7 +10,28 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 导出  表 wechat.shiro_permissions 结构
+-- 导出  表 eastbride.eastbride_carousel 结构
+DROP TABLE IF EXISTS `eastbride_carousel`;
+CREATE TABLE IF NOT EXISTS `eastbride_carousel` (
+  `crs_id` int(11) NOT NULL AUTO_INCREMENT,
+  `crs_h2` varchar(255) DEFAULT NULL,
+  `crs_p` text,
+  `crs_img` text,
+  `crs_href` text,
+  `crs_button` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`crs_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- 正在导出表  eastbride.eastbride_carousel 的数据：~2 rows (大约)
+DELETE FROM `eastbride_carousel`;
+/*!40000 ALTER TABLE `eastbride_carousel` DISABLE KEYS */;
+INSERT INTO `eastbride_carousel` (`crs_id`, `crs_h2`, `crs_p`, `crs_img`, `crs_href`, `crs_button`) VALUES
+	(1, '美轮美奂', '用心去拍，留下最美的瞬间！', 'static/modules/home/img/slide-3.jpg', '', '关注'),
+	(2, '唯你独尊', '客户至上，给你最好的服务！', 'static/modules/home/img/slide-4.jpg', NULL, '订阅');
+/*!40000 ALTER TABLE `eastbride_carousel` ENABLE KEYS */;
+
+
+-- 导出  表 eastbride.shiro_permissions 结构
 DROP TABLE IF EXISTS `shiro_permissions`;
 CREATE TABLE IF NOT EXISTS `shiro_permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `shiro_permissions` (
   UNIQUE KEY `idx_permissions_permission` (`permission`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_permissions 的数据：~3 rows (大约)
+-- 正在导出表  eastbride.shiro_permissions 的数据：~2 rows (大约)
 DELETE FROM `shiro_permissions`;
 /*!40000 ALTER TABLE `shiro_permissions` DISABLE KEYS */;
 INSERT INTO `shiro_permissions` (`id`, `permission`, `permission_desc`) VALUES
@@ -29,7 +50,7 @@ INSERT INTO `shiro_permissions` (`id`, `permission`, `permission_desc`) VALUES
 /*!40000 ALTER TABLE `shiro_permissions` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shiro_roles 结构
+-- 导出  表 eastbride.shiro_roles 结构
 DROP TABLE IF EXISTS `shiro_roles`;
 CREATE TABLE IF NOT EXISTS `shiro_roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -39,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `shiro_roles` (
   UNIQUE KEY `idx_roles_role` (`role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_roles 的数据：~3 rows (大约)
+-- 正在导出表  eastbride.shiro_roles 的数据：~3 rows (大约)
 DELETE FROM `shiro_roles`;
 /*!40000 ALTER TABLE `shiro_roles` DISABLE KEYS */;
 INSERT INTO `shiro_roles` (`id`, `role`, `role_desc`) VALUES
@@ -49,7 +70,7 @@ INSERT INTO `shiro_roles` (`id`, `role`, `role_desc`) VALUES
 /*!40000 ALTER TABLE `shiro_roles` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shiro_roles_permissions 结构
+-- 导出  表 eastbride.shiro_roles_permissions 结构
 DROP TABLE IF EXISTS `shiro_roles_permissions`;
 CREATE TABLE IF NOT EXISTS `shiro_roles_permissions` (
   `role_id` int(11) NOT NULL,
@@ -57,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `shiro_roles_permissions` (
   PRIMARY KEY (`role_id`,`permission_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_roles_permissions 的数据：~4 rows (大约)
+-- 正在导出表  eastbride.shiro_roles_permissions 的数据：~4 rows (大约)
 DELETE FROM `shiro_roles_permissions`;
 /*!40000 ALTER TABLE `shiro_roles_permissions` DISABLE KEYS */;
 INSERT INTO `shiro_roles_permissions` (`role_id`, `permission_id`) VALUES
@@ -68,7 +89,7 @@ INSERT INTO `shiro_roles_permissions` (`role_id`, `permission_id`) VALUES
 /*!40000 ALTER TABLE `shiro_roles_permissions` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shiro_urls 结构
+-- 导出  表 eastbride.shiro_urls 结构
 DROP TABLE IF EXISTS `shiro_urls`;
 CREATE TABLE IF NOT EXISTS `shiro_urls` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -82,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `shiro_urls` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_urls 的数据：~5 rows (大约)
+-- 正在导出表  eastbride.shiro_urls 的数据：~6 rows (大约)
 DELETE FROM `shiro_urls`;
 /*!40000 ALTER TABLE `shiro_urls` DISABLE KEYS */;
 INSERT INTO `shiro_urls` (`id`, `permission_id`, `url_type_id`, `url`, `text`, `icon`, `url_order`, `is_iframe`) VALUES
@@ -95,7 +116,7 @@ INSERT INTO `shiro_urls` (`id`, `permission_id`, `url_type_id`, `url`, `text`, `
 /*!40000 ALTER TABLE `shiro_urls` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shiro_urls_type 结构
+-- 导出  表 eastbride.shiro_urls_type 结构
 DROP TABLE IF EXISTS `shiro_urls_type`;
 CREATE TABLE IF NOT EXISTS `shiro_urls_type` (
   `url_type_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -104,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `shiro_urls_type` (
   PRIMARY KEY (`url_type_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_urls_type 的数据：~2 rows (大约)
+-- 正在导出表  eastbride.shiro_urls_type 的数据：~2 rows (大约)
 DELETE FROM `shiro_urls_type`;
 /*!40000 ALTER TABLE `shiro_urls_type` DISABLE KEYS */;
 INSERT INTO `shiro_urls_type` (`url_type_id`, `url_type_name`, `url_type_icon`) VALUES
@@ -113,7 +134,7 @@ INSERT INTO `shiro_urls_type` (`url_type_id`, `url_type_name`, `url_type_icon`) 
 /*!40000 ALTER TABLE `shiro_urls_type` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shiro_users 结构
+-- 导出  表 eastbride.shiro_users 结构
 DROP TABLE IF EXISTS `shiro_users`;
 CREATE TABLE IF NOT EXISTS `shiro_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -125,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `shiro_users` (
   UNIQUE KEY `idx_users_username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_users 的数据：~3 rows (大约)
+-- 正在导出表  eastbride.shiro_users 的数据：~3 rows (大约)
 DELETE FROM `shiro_users`;
 /*!40000 ALTER TABLE `shiro_users` DISABLE KEYS */;
 INSERT INTO `shiro_users` (`id`, `username`, `password`, `salt`, `locked`) VALUES
@@ -135,7 +156,7 @@ INSERT INTO `shiro_users` (`id`, `username`, `password`, `salt`, `locked`) VALUE
 /*!40000 ALTER TABLE `shiro_users` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shiro_users_roles 结构
+-- 导出  表 eastbride.shiro_users_roles 结构
 DROP TABLE IF EXISTS `shiro_users_roles`;
 CREATE TABLE IF NOT EXISTS `shiro_users_roles` (
   `user_id` int(11) NOT NULL,
@@ -143,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `shiro_users_roles` (
   PRIMARY KEY (`user_id`,`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shiro_users_roles 的数据：~3 rows (大约)
+-- 正在导出表  eastbride.shiro_users_roles 的数据：~3 rows (大约)
 DELETE FROM `shiro_users_roles`;
 /*!40000 ALTER TABLE `shiro_users_roles` DISABLE KEYS */;
 INSERT INTO `shiro_users_roles` (`user_id`, `role_id`) VALUES
@@ -153,7 +174,7 @@ INSERT INTO `shiro_users_roles` (`user_id`, `role_id`) VALUES
 /*!40000 ALTER TABLE `shiro_users_roles` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shop_goods 结构
+-- 导出  表 eastbride.shop_goods 结构
 DROP TABLE IF EXISTS `shop_goods`;
 CREATE TABLE IF NOT EXISTS `shop_goods` (
   `goods_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -166,13 +187,13 @@ CREATE TABLE IF NOT EXISTS `shop_goods` (
   PRIMARY KEY (`goods_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shop_goods 的数据：~0 rows (大约)
+-- 正在导出表  eastbride.shop_goods 的数据：~0 rows (大约)
 DELETE FROM `shop_goods`;
 /*!40000 ALTER TABLE `shop_goods` DISABLE KEYS */;
 /*!40000 ALTER TABLE `shop_goods` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shop_menu 结构
+-- 导出  表 eastbride.shop_menu 结构
 DROP TABLE IF EXISTS `shop_menu`;
 CREATE TABLE IF NOT EXISTS `shop_menu` (
   `menu_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -180,13 +201,13 @@ CREATE TABLE IF NOT EXISTS `shop_menu` (
   PRIMARY KEY (`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shop_menu 的数据：~0 rows (大约)
+-- 正在导出表  eastbride.shop_menu 的数据：~0 rows (大约)
 DELETE FROM `shop_menu`;
 /*!40000 ALTER TABLE `shop_menu` DISABLE KEYS */;
 /*!40000 ALTER TABLE `shop_menu` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shop_order 结构
+-- 导出  表 eastbride.shop_order 结构
 DROP TABLE IF EXISTS `shop_order`;
 CREATE TABLE IF NOT EXISTS `shop_order` (
   `order_id` varchar(100) NOT NULL,
@@ -202,13 +223,13 @@ CREATE TABLE IF NOT EXISTS `shop_order` (
   PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shop_order 的数据：~0 rows (大约)
+-- 正在导出表  eastbride.shop_order 的数据：~0 rows (大约)
 DELETE FROM `shop_order`;
 /*!40000 ALTER TABLE `shop_order` DISABLE KEYS */;
 /*!40000 ALTER TABLE `shop_order` ENABLE KEYS */;
 
 
--- 导出  表 wechat.shop_wifi 结构
+-- 导出  表 eastbride.shop_wifi 结构
 DROP TABLE IF EXISTS `shop_wifi`;
 CREATE TABLE IF NOT EXISTS `shop_wifi` (
   `openId` varchar(100) NOT NULL,
@@ -217,7 +238,7 @@ CREATE TABLE IF NOT EXISTS `shop_wifi` (
   PRIMARY KEY (`openId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.shop_wifi 的数据：~0 rows (大约)
+-- 正在导出表  eastbride.shop_wifi 的数据：~0 rows (大约)
 DELETE FROM `shop_wifi`;
 /*!40000 ALTER TABLE `shop_wifi` DISABLE KEYS */;
 INSERT INTO `shop_wifi` (`openId`, `captcha`, `expired_dt`) VALUES
@@ -225,7 +246,7 @@ INSERT INTO `shop_wifi` (`openId`, `captcha`, `expired_dt`) VALUES
 /*!40000 ALTER TABLE `shop_wifi` ENABLE KEYS */;
 
 
--- 导出  表 wechat.sys_config 结构
+-- 导出  表 eastbride.sys_config 结构
 DROP TABLE IF EXISTS `sys_config`;
 CREATE TABLE IF NOT EXISTS `sys_config` (
   `cfg_key` varchar(50) NOT NULL,
@@ -234,7 +255,7 @@ CREATE TABLE IF NOT EXISTS `sys_config` (
   PRIMARY KEY (`cfg_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.sys_config 的数据：~8 rows (大约)
+-- 正在导出表  eastbride.sys_config 的数据：~8 rows (大约)
 DELETE FROM `sys_config`;
 /*!40000 ALTER TABLE `sys_config` DISABLE KEYS */;
 INSERT INTO `sys_config` (`cfg_key`, `cfg_value`, `cfg_type_id`) VALUES
@@ -249,7 +270,7 @@ INSERT INTO `sys_config` (`cfg_key`, `cfg_value`, `cfg_type_id`) VALUES
 /*!40000 ALTER TABLE `sys_config` ENABLE KEYS */;
 
 
--- 导出  表 wechat.sys_config_type 结构
+-- 导出  表 eastbride.sys_config_type 结构
 DROP TABLE IF EXISTS `sys_config_type`;
 CREATE TABLE IF NOT EXISTS `sys_config_type` (
   `cfg_type_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -257,7 +278,7 @@ CREATE TABLE IF NOT EXISTS `sys_config_type` (
   PRIMARY KEY (`cfg_type_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.sys_config_type 的数据：~3 rows (大约)
+-- 正在导出表  eastbride.sys_config_type 的数据：~3 rows (大约)
 DELETE FROM `sys_config_type`;
 /*!40000 ALTER TABLE `sys_config_type` DISABLE KEYS */;
 INSERT INTO `sys_config_type` (`cfg_type_id`, `cfg_type_name`) VALUES
@@ -267,7 +288,7 @@ INSERT INTO `sys_config_type` (`cfg_type_id`, `cfg_type_name`) VALUES
 /*!40000 ALTER TABLE `sys_config_type` ENABLE KEYS */;
 
 
--- 导出  表 wechat.wx_customer 结构
+-- 导出  表 eastbride.wx_customer 结构
 DROP TABLE IF EXISTS `wx_customer`;
 CREATE TABLE IF NOT EXISTS `wx_customer` (
   `openId` varchar(50) NOT NULL,
@@ -283,7 +304,7 @@ CREATE TABLE IF NOT EXISTS `wx_customer` (
   PRIMARY KEY (`openId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  wechat.wx_customer 的数据：~1 rows (大约)
+-- 正在导出表  eastbride.wx_customer 的数据：~0 rows (大约)
 DELETE FROM `wx_customer`;
 /*!40000 ALTER TABLE `wx_customer` DISABLE KEYS */;
 INSERT INTO `wx_customer` (`openId`, `subscribe_flag`, `true_name`, `mobile`, `address`, `money`, `create_id`, `create_dt`, `update_id`, `update_dt`) VALUES
@@ -291,7 +312,26 @@ INSERT INTO `wx_customer` (`openId`, `subscribe_flag`, `true_name`, `mobile`, `a
 /*!40000 ALTER TABLE `wx_customer` ENABLE KEYS */;
 
 
--- 导出  表 wechat.wx_props 结构
+-- 导出  表 eastbride.wx_dev_config 结构
+DROP TABLE IF EXISTS `wx_dev_config`;
+CREATE TABLE IF NOT EXISTS `wx_dev_config` (
+  `appId` varchar(50) NOT NULL,
+  `appSecret` varchar(50) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `messageEncrypt` tinyint(1) NOT NULL,
+  `encodingAesKey` varchar(255) NOT NULL,
+  PRIMARY KEY (`appId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='微信配置表';
+
+-- 正在导出表  eastbride.wx_dev_config 的数据：~0 rows (大约)
+DELETE FROM `wx_dev_config`;
+/*!40000 ALTER TABLE `wx_dev_config` DISABLE KEYS */;
+INSERT INTO `wx_dev_config` (`appId`, `appSecret`, `token`, `messageEncrypt`, `encodingAesKey`) VALUES
+	('wxbbaed5839238c4eb', '064682c9add7e6756f9f435c904825a9', 'eastbride', 0, 'RBSOCvhUnTljEvosRNwwek2NB6wIuqI2B4sVNpM3Ni6');
+/*!40000 ALTER TABLE `wx_dev_config` ENABLE KEYS */;
+
+
+-- 导出  表 eastbride.wx_props 结构
 DROP TABLE IF EXISTS `wx_props`;
 CREATE TABLE IF NOT EXISTS `wx_props` (
   `appId` varchar(50) NOT NULL,
@@ -302,7 +342,7 @@ CREATE TABLE IF NOT EXISTS `wx_props` (
   `userId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='微信配置表';
 
--- 正在导出表  wechat.wx_props 的数据：~1 rows (大约)
+-- 正在导出表  eastbride.wx_props 的数据：~0 rows (大约)
 DELETE FROM `wx_props`;
 /*!40000 ALTER TABLE `wx_props` DISABLE KEYS */;
 INSERT INTO `wx_props` (`appId`, `appSecret`, `token`, `messageEncrypt`, `encodingAesKey`, `userId`) VALUES
