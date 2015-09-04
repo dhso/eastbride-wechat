447c193754d47bf6bab08e1cb564c70c305458b9
hadong <hadong@hadong-pc>
2015-09-04 18:54:08 +0800

    ++

--- src/main/java/modules/eastbride/controller/EastbrideController.java
+++ src/main/java/modules/eastbride/controller/EastbrideController.java
@@ -1,11 +1,14 @@
 package modules.eastbride.controller;
 
-import modules.eastbride.model.EastbrideModel;
-
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 
 import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
+
+import frame.plugin.easyui.DataGrid;
+import modules.eastbride.model.EastbrideModel;
 
 public class EastbrideController extends Controller {
 
@@ -14,11 +17,35 @@ public class EastbrideController extends Controller {
 		setAttr("galleryList", EastbrideModel.dao.getGallery());
 		render("index.htm");
 	}
-	
+
 	@RequiresAuthentication
 	@ActionKey("eastbride/carousel/manage")
 	public void carouselManage() {
-		render("weixin-customer.htm");
+		render("eastbride-carousel.htm");
+	}
+
+	@RequiresAuthentication
+	@ActionKey("eastbride/carousel/get")
+	public void carouselGet() {
+		Integer pageNumber = getParaToInt("page", 1);
+		Integer pageSize = getParaToInt("rows", 10);
+		Page<EastbrideModel> carousel = EastbrideModel.dao.getCarouselPage(pageNumber, pageSize);
+		renderJson(new DataGrid(String.valueOf(carousel.getTotalRow()), carousel.getList()));
+	}
+
+	@RequiresAuthentication
+	@ActionKey("eastbride/gallery/manage")
+	public void galleryManage() {
+		render("eastbride-gallery.htm");
+	}
+
+	@RequiresAuthentication
+	@ActionKey("eastbride/gallery/get")
+	public void galleryGet() {
+		Integer pageNumber = getParaToInt("page", 1);
+		Integer pageSize = getParaToInt("rows", 10);
+		Page<Record> gallery = EastbrideModel.dao.getGalleryPage(pageNumber, pageSize);
+		renderJson(new DataGrid(String.valueOf(gallery.getTotalRow()), gallery.getList()));
 	}
 
 }
--- src/main/java/modules/eastbride/model/EastbrideModel.java
+++ src/main/java/modules/eastbride/model/EastbrideModel.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
 
 import frame.plugin.tablebind.TableBind;
@@ -23,12 +24,34 @@ public class EastbrideModel extends Model<EastbrideModel> {
 	}
 
 	/**
+	 * 分页获取轮播
+	 * 
+	 * @param pageNumber
+	 * @param pageSize
+	 * @return
+	 */
+	public Page<EastbrideModel> getCarouselPage(int pageNumber, int pageSize) {
+		return EastbrideModel.dao.paginate(pageNumber, pageSize, "select *", "from eastbride_carousel");
+	}
+
+	/**
 	 * 获取图集
 	 * 
 	 * @return
 	 */
 	public List<Record> getGallery() {
 		return Db.find("select * from eastbride_gallery");
+	}
+
+	/**
+	 * 分页获取图集
+	 * 
+	 * @param pageNumber
+	 * @param pageSize
+	 * @return
+	 */
+	public Page<Record> getGalleryPage(int pageNumber, int pageSize) {
+		return Db.paginate(pageNumber, pageSize, "select *", "from eastbride_gallery");
 	}
 
 }
